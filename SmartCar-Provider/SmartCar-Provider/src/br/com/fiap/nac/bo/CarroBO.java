e4f26807b7efa601b3dfa2fb67bd720100154949
Douglas Carvalho <douglascarvalhoromano@gmail.com>
2016-05-02 22:13:04 -0300

    AJUDA AI PESSOAL CRIEI A BO AJUDA A IMPLEMENTAR A LOGICA BORA TERMINAR SA BAGAÇA

---
+++ SmartCar-Provider/SmartCar-Provider/src/br/com/fiap/nac/bo/CarroBO.java
@@ -0,0 +1,42 @@
+package br.com.fiap.nac.bo;
+
+import javax.persistence.EntityManager;
+
+import br.com.fiap.nac.dao.CarroDAO;
+import br.com.fiap.nac.dao.impl.CarroDAOImpl;
+import br.com.fiap.nac.entity.Carro;
+import br.com.fiap.nac.exception.DBException;
+import br.com.fiap.nac.exception.EntityNotFoundException;
+import br.com.fiap.nac.singleton.EntityManagerFactorySingleton;
+
+public class CarroBO {
+
+	private static EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
+
+	public void cadastrar(Carro carro) {
+		try {
+			if (carro.getAno() > 2005) {
+				CarroDAO carroDao = new CarroDAOImpl(em);
+				carroDao.cadastrar(carro);
+			} else {
+				System.out.println("Testando BO");
+			}
+		} catch (DBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void Buscar(long chave) {
+		try {
+			CarroDAO carroDAO = new CarroDAOImpl(em);
+			carroDAO.buscar(chave);
+		} catch (EntityNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void ValidarKm(long kmAtual) {
+
+	}
+
+}
